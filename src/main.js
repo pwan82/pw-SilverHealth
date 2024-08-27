87863888fbced1ccffac9df0707e53705c7af7eb
pwan82 <pwan0082@student.monash.edu>
2024-08-27 23:33:12 +1000

    Add dependencies

--- src/main.js
+++ src/main.js
@@ -3,4 +3,18 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/Column'
+
+const app = createApp(App)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
+
+app.mount('#app')
