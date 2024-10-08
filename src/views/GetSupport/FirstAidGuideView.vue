3ea54ba1c92886ef457ee8a4ab511125cfcce167
pwan82 <pwan0082@student.monash.edu>
2024-10-08 13:59:20 +1100

    Using ArticleDetailView to show public pages

--- src/views/GetSupport/FirstAidGuideView.vue
+++ src/views/GetSupport/FirstAidGuideView.vue
@@ -1,12 +1,7 @@
 <template>
-  <div class="first-aid-guide">
-    <div class="container mt-5">
-      <div class="row">
-        <div class="col-md-8 offset-md-2">
-          <h1 class="text-center">First Aid Guide</h1>
-          <p class="text-center">1. Call 000</p>
-        </div>
-      </div>
-    </div>
-  </div>
+  <ArticleDetailView :articleId="10" />
 </template>
+
+<script setup>
+import ArticleDetailView from '@/views/Article/ArticleDetailView.vue'
+</script>
